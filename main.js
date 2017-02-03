f0100a7d98930c94f54ee11d15f6ff0c143dd1d9
latifakha <khachchabi.latifa@gmail.com>
2017-02-03 11:35:19 +0100

    ajout des nouveaux fichier

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
+Vue.component('task-list', {
+	template:`
+		<div> 
+			<task v-for="task in tasks">{{ task.task }}</task>
+		</div>`,
+//il faut utililiser `` et nn '' et je sais pas pourquoi -_-
+	data(){
+		return {
+			tasks:[
+				{ task: 'task 1', complete: false},
+				{ task: 'task 2', complete:true },
+				{ task: 'task 3', complete:true },
+				{ task: 'task 4', complete: false}
+			]
+		};
+	}
+});
+
+
 Vue.component('task',{
 	//template: '<li> Foobar </li>'
 	template: '<li> <slot> </slot> </li>',
 
-	 /*data(){
-	 	return {
-	 		message: 'Foobar'
-
-	 	};
-	 }*/
+	 /*data(){return { message: 'Foobar'};}*/
 });
 
 new Vue({
